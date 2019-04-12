4ba244bc265c58f93f6d799f5d4d9ff6d40bd6fb
cmik <cmik_21@yahoo.fr>
2019-04-12 09:49:27 +0200

    Tfc.tv version 1.2.4     Fixed version 1.2.3 upgrade when first or new install

--- plugin.video.tfctv/resources/upgrade.py
+++ plugin.video.tfctv/resources/upgrade.py
@@ -16,7 +16,8 @@ logger = control.logger
 
 def upgradeDB():
 
-    # Load DB
+    # Always check if DB is installed
+    tools.checkInstallDB()
 
     # DB upgrades per version
     if control.setting('lastVersion') == '1.0.0-beta' and control.addonInfo('version') == '1.0.0':
@@ -36,15 +37,16 @@ def upgradeDB():
         # Check if installation is complete
         logger.logNotice('Checking installation')
         tools.checkInstallDB(True)
-    elif control.addonInfo('version') == '1.2.3':
-        logger.logNotice('Updating version 1.2.3')
+    elif control.addonInfo('version') in ('1.2.3', '1.2.4'):
+        logger.logNotice('Updating version %s' % control.addonInfo('version'))
         episodeDB = episodes.Episode(control.episodesFile)
         showDB = shows.Show(control.showsFile)
         showViews = logger.logInfo(episodeDB.execute(['select showid, sum(views) from EPISODE where views > 0 group by showid']))
         updateQueries = []
-        for data in showViews[0]:
-            updateQueries.append('UPDATE SHOW SET VIEWS = %d WHERE ID = %d' % (int(data[1]), int(data[0])))
-        showDB.executeUpdate(logger.logInfo(updateQueries))
+        if (showViews and len(showViews) > 0):
+            for data in showViews[0]:
+                updateQueries.append('UPDATE SHOW SET VIEWS = %d WHERE ID = %d' % (int(data[1]), int(data[0])))
+            showDB.executeUpdate(logger.logInfo(updateQueries))
 
 
         
