0eb9955120767d54cda08437de4d66e9f7b51ade
cmik <cmik_21@yahoo.fr>
2020-02-08 17:33:05 +0100

    Tfc.tv version 1.4.7     Fixed login issue (HTTP 307 error)

--- plugin.video.tfctv/resources/lib/sources/tfctv.py
+++ plugin.video.tfctv/resources/lib/sources/tfctv.py
@@ -1615,7 +1615,17 @@ def loginToWebsite(quiet=False, login=False, password=False):
     ocpKey = common.parseDOM(signin, "sso-accounts", ret = 'api-key')[0]
 
     # Retrieve apiKey
-    data = callJsonApi(config.uri.get('apiKey') % ocpKey, base_url=config.apiSSOUrl, useCache = False, jsonData=True)
+    data = callJsonApi(
+        config.uri.get('apiKey') % ocpKey,
+        base_url=config.apiAzureUrl,
+        headers = [
+            ('Ocp-Apim-Subscription-Key', ocpKey),
+            ('Origin', config.kapamilyaAccountsSSOUrl),
+            ('Referer', config.kapamilyaAccountsSSOUrl + config.uri.get('signin'))
+            ], 
+        useCache = False,
+        jsonData=True
+    )
     if (not data or ('apiKey' not in data)):
         control.showNotification(control.lang(57024), control.lang(50006))
     else:
